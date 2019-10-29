2a3832ca2dc3a6ff33c396849b7847a615611789
FrancyLeon <sala@HOR-SALA501-011.unihorizonte.salas>
2019-10-29 16:57:57 -0500

    color

--- SenaSalud/src/senasalud/SenaSalud.java
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -9,10 +9,12 @@ public class SenaSalud {
     
     public static void main(String[] args) {
         String apellido=JOptionPane.showInputDialog(null,"Digite su apellido");
-        String nombre=(JOptionPane.showInputDialog(null,"Digite su apellido"));
+        String nombre=(JOptionPane.showInputDialog(null,"Digite su nombre"));
+        String color=JOptionPane.showInputDialog(null,"Digite el color");
         
         System.out.println("su apellido es: "+apellido);
         System.out.println("su edad es: "+nombre);
+        System.out.println("su color es: "+color);
     }
     
 }
