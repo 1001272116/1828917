aa25372a5e3e5e11d614658aac31ce5899772dec
FrancyLeon <sala@HOR-SALA501-011.unihorizonte.salas>
2019-10-29 15:42:56 -0500

    algo

---
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -0,0 +1,16 @@
+
+package senasalud;
+
+import javax.swing.JOptionPane;
+
+
+public class SenaSalud {
+
+    
+    public static void main(String[] args) {
+        String nombre=JOptionPane.showInputDialog(null,"Digite su nombre");
+        
+        System.out.println("su nombre es: "+nombre);
+    }
+    
+}
