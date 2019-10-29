55b977609db8c6cc6fa408a764d407353ce603c1
FrancyLeon <sala@HOR-SALA501-011.unihorizonte.salas>
2019-10-29 16:53:07 -0500

    Datos de ingreso

--- SenaSalud/src/senasalud/SenaSalud.java
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -9,9 +9,10 @@ public class SenaSalud {
     
     public static void main(String[] args) {
         String apellido=JOptionPane.showInputDialog(null,"Digite su apellido");
+        String nombre=(JOptionPane.showInputDialog(null,"Digite su apellido"));
         
         System.out.println("su apellido es: "+apellido);
-        
+        System.out.println("su edad es: "+nombre);
     }
     
 }
